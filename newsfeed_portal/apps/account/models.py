24cb3da670383ead6f79c7a344e71574368a24c5
Adnan Alam <adnan.alam@rfera.asia>
2022-06-30 00:55:45 +0600

    added User model in account app

--- newsfeed_portal/apps/account/models.py
+++ newsfeed_portal/apps/account/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class User(AbstractUser):
+    username_validator = UnicodeUsernameValidator()
+
+    name = models.CharField(max_length=255, blank=True)
+    email = models.EmailField(max_length=255, unique=True)
+    username = models.CharField(
+        max_length=255,
+        unique=True,
+        validators=[username_validator],
+        help_text=_(
+            "Required. 255 characters or fewer. Letters, digits and @/./+/-/_ only"
+        ),
+        error_messages={"unique": _("A user with that username already exists")},
+    )
+
+    def __str__(self):
+        return self.username
