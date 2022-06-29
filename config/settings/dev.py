1cae070e4bfb20bf5fb30db086b93170720b1271
Adnan Alam <adnan.alam@rfera.asia>
2022-06-30 00:44:12 +0600

    project restructured

---
+++ config/settings/dev.py
@@ -0,0 +1,10 @@
+from .base import *
+
+
+DEBUG = True
+
+ALLOWED_HOSTS = ["*"]
+
+CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:8080", "http://localhost:8080"]
+
+INTERNAL_IPS = ["127.0.0.1"]
---
+++ config/settings/prod.py
@@ -0,0 +1,10 @@
+from .base import *
+
+
+DEBUG = False
+
+ALLOWED_HOSTS = ["*"]
+
+CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:8080"]
+
+INTERNAL_IPS = ["127.0.0.1"]
