8c6e2be9e37b049aff921cea122f3d95fdfac446
Adnan Alam <adnan.alam@rfera.asia>
2022-06-30 01:43:46 +0600

    included account app's urls in base urls.py

--- newsfeed_portal/urls.py
+++ newsfeed_portal/urls.py
@@ -19,11 +19,12 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/v1/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/v1/", include("newsfeed_portal.apps.account.api.urls")),
 ]
