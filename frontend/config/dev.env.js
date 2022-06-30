47e5734dd7786097c91c4ff6423c085f5b04decb
Adnan Alam <adnan.alam@rfera.asia>
2022-07-01 00:32:39 +0600

    changed the backend base URL

--- frontend/config/dev.env.js
+++ frontend/config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require("./prod.env");
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_URL: '"http://127.0.0.1:8020"'
+  BASE_URL: '"http://127.0.0.1:8000"'
 });
--- frontend/config/prod.env.js
+++ frontend/config/prod.env.js
@@ -1,5 +1,5 @@
 "use strict";
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_URL: '"http://127.0.0.1:8020"'
+  BASE_URL: '"http://127.0.0.1:8000"'
 };
