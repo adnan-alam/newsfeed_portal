17d38021fe03e21b27e44c6645dd345f233a8cac
Adnan Alam <adnan.alam@rfera.asia>
2022-07-01 00:10:22 +0600

    added NewsSourceSerializer and NewsSettingsSerializer

--- newsfeed_portal/apps/news/api/serializers.py
+++ newsfeed_portal/apps/news/api/serializers.py
@@ -11,3 +11,18 @@ class NewsSerializer(DynamicFieldsModelSerializer):
         model = models_news.News
         fields = ["id", "headline", "thumbnail", "news_source", "country", "news_url"]
         read_only_fields = fields
+
+
+class NewsSourceSerializer(DynamicFieldsModelSerializer):
+    class Meta:
+        model = models_news.NewsSource
+        fields = ["id", "name", "slug", "country"]
+        read_only_fields = fields
+
+
+class NewsSettingsSerializer(DynamicFieldsModelSerializer):
+    id = serializers.IntegerField(required=False)
+
+    class Meta:
+        model = models_news.NewsSettings
+        fields = ["id", "country", "source", "keywords"]
