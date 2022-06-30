7af3c133c3e9eeebce449ac35b661b98bd66c83d
Adnan Alam <adnan.alam@rfera.asia>
2022-06-30 21:04:16 +0600

    fixed CORS_ALLOWED_ORIGINS

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -5,6 +5,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:8080", "http://localhost:8080"]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
+]
 
 INTERNAL_IPS = ["127.0.0.1"]
